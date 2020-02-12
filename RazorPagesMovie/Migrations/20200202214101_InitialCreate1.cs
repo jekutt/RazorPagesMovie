cb2fc1d2692a11509db27347b3ecc43ea4f7400e
Jegor Kütt <jekutt@ttu.ee>
2020-02-12 17:33:16 +0200

    HW1

---
+++ RazorPagesMovie/Migrations/20200202214101_InitialCreate1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prise",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "RealeseDate",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Prise",
+                table: "Movie",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RealeseDate",
+                table: "Movie",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
